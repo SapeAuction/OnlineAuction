7aa5e25ab662dd1b2fdbecc4d19c0617a74036da
smosa <sudhakarraju.mosa@xinthe.com>
2016-06-27 00:38:30 -0700

    Login changes

---
+++ Auction.WebApi.Sevices/Implementations/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Auction.Entity;
+
+namespace Auction.WebApi.Sevices.Implementations
+{
+    public interface IUserService
+    {
+        int CreateUser(User userEntity);
+        bool DeleteUser(int userEntityId);
+        IEnumerable<User> GetAllUsers();
+        User GetUserById(string userName);
+        User IsValidUser(string userName, string userPassword);
+        bool UpdateUser(int uid, User userUpdatedEntity);
+    }
+}
